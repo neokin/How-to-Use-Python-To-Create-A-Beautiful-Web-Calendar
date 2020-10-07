46f6ed9c63ff149942856132d01d7387c35a1d6c
kirill <mazkirand@gmail.com>
2020-10-07 22:53:27 +0300

    Add navigation

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-"""
-    @Author:		Мазанов Кирилл 
-    @Created:		06.10.2020
-    
-"""
+from flask import Flask, render_template, redirect
+from flask_nav import Nav
+from flask_nav.elements import *
+
 import generatePayDayWebpage
-from flask import Flask, render_template, redirect, url_for
+
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+topbar = Navbar('Меню', View('Главная', 'hello'), View('Обеты', 'about'),)
+nav = Nav()
+nav.register_element('left', topbar)
+nav.init_app(app)
+
+
 
 @app.errorhandler(404)
 def page_not_found(error=None):
     print("404 WASROUT")
     return redirect(url_for('hello'))
 
+@app.route('/about')
+def about(name=None):
+    print("WASROUT")
+#    generatePayDayWebpage.generatewebpage()
+    return render_template('about.html', name=name)
 
 @app.route('/')
 def hello(name=None):
@@ -23,8 +34,8 @@ def hello(name=None):
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=80)
-    app.run()
+    app.run(host='0.0.0.0', port=80)
+    #app.run()
 
 
 
