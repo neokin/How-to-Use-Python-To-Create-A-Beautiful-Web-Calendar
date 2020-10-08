f2fa3d188e33eb446c1b020253965a498ac8ad57
kirill <mazkirand@gmail.com>
2020-10-08 20:36:08 +0300

    Add FAQ page

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import generatePayDayWebpage
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-topbar = Navbar('Меню', View('Главная', 'hello'), View('Обеты', 'about'),)
+topbar = Navbar('Меню', View('Главная', 'hello'), View('Обеты', 'about'),  View('ЧАВО', 'faq'))
 nav = Nav()
 nav.register_element('left', topbar)
 nav.init_app(app)
@@ -16,26 +16,24 @@ nav.init_app(app)
 
 @app.errorhandler(404)
 def page_not_found(error=None):
-    print("404 WASROUT")
     return redirect(url_for('hello'))
 
 @app.route('/about')
 def about(name=None):
-    print("WASROUT")
-#    generatePayDayWebpage.generatewebpage()
     return render_template('about.html', name=name)
 
 @app.route('/')
 def hello(name=None):
-    print("WASROUT")
     generatePayDayWebpage.generatewebpage()
     return render_template('index.html', name=name)
 
-
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-    #app.run()
+    #app.run(host='0.0.0.0', port=80)
+    app.run()
 
 
 
